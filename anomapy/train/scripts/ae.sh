68bcc331d357890e0c3ef507c170d4a2c4688840
Benedict Wilkins <=brjw@hotmail.co.uk>
2020-01-30 12:42:28 +0000

    added AWS test file

--- anomapy/train/scripts/ae.sh
+++ anomapy/train/scripts/ae.sh
@@ -1,7 +1,7 @@
-python -m anomapy.train.ae -env BeamRider -epochs 12
-#python -m anomapy.train.ae -env Breakout -epochs 12
-#python -m anomapy.train.ae -env Enduro -epochs 12
-#python -m anomapy.train.ae -env Pong -epochs 20
-#python -m anomapy.train.ae -env Qbert -epochs 20
-#python -m anomapy.train.ae -env Seaquest -epochs 20
-#python -m anomapy.train.ae -env SpaceInvaders -epochs 20
+python -m anomapy.train.ae -env BeamRider -epochs 12 -batch_size 256
+python -m anomapy.train.ae -env Breakout -epochs 12 -batch_size 256
+python -m anomapy.train.ae -env Enduro -epochs 12 -batch_size 256
+python -m anomapy.train.ae -env Pong -epochs 12 -batch_size 256
+python -m anomapy.train.ae -env Qbert -epochs 12 -batch_size 256
+python -m anomapy.train.ae -env Seaquest -epochs 12 -batch_size 256
+python -m anomapy.train.ae -env SpaceInvaders -epochs 12 -batch_size 256
---
+++ anomapy/train/test.py
@@ -0,0 +1,6 @@
+print("TESTING DEPENDANCIES:")
+import pyworld.toolkit.tools.torchutils as tu
+print("SUCCESS")
+
+print("TESTING DEVICE AVAILABILITY:")
+tu.device()
