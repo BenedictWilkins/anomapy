a3faa392428906a4276216e3be677bd2a5616e5b
Benedict Wilkins <=brjw@hotmail.co.uk>
2020-02-05 10:24:45 +0000

    training script changes

--- anomapy/train/scripts/ae.sh
+++ anomapy/train/scripts/ae.sh
@@ -1,7 +1,7 @@
-python -m anomapy.train.ae -env BeamRider -epochs 12 -batch_size 256
-python -m anomapy.train.ae -env Breakout -epochs 12 -batch_size 256
-python -m anomapy.train.ae -env Enduro -epochs 12 -batch_size 256
-python -m anomapy.train.ae -env Pong -epochs 12 -batch_size 256
-python -m anomapy.train.ae -env Qbert -epochs 12 -batch_size 256
-python -m anomapy.train.ae -env Seaquest -epochs 12 -batch_size 256
-python -m anomapy.train.ae -env SpaceInvaders -epochs 12 -batch_size 256
+python -m anomapy.train.ae -env BeamRider -epochs 12 -batch_size 128 -colour True -latent_shape 64
+python -m anomapy.train.ae -env Breakout -epochs 12 -batch_size 128 -colour True -latent_shape 64
+python -m anomapy.train.ae -env Enduro -epochs 12 -batch_size 128 -colour True -latent_shape 64
+python -m anomapy.train.ae -env Pong -epochs 12 -batch_size  -colour True -latent_shape 64
+python -m anomapy.train.ae -env Qbert -epochs 12 -batch_size 128 -colour True -latent_shape 64
+python -m anomapy.train.ae -env Seaquest -epochs 12 -batch_size 128 -colour True -latent_shape 64
+python -m anomapy.train.ae -env SpaceInvaders -epochs 12 -batch_size 128 -colour True -latent_shape 64
